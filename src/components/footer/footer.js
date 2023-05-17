671adcf4d44d3057074d44c6821deaeb5b69ad87
henriqueAlgauer <algauer.henrique@gmail.com>
2023-05-17 17:06:47 -0300

    add readme

--- src/components/footer/footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className='p-8 flex  justify-center items-end text-zinc-100'>
-            <p>Copyright ©2023 Henrique Alagauer </p>
-        </footer>
-    )
-}
-
-export {Footer}
