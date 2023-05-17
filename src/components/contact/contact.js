9f8c80cf5b701043cf94b82ab2937356861e9d4b
henriqueAlgauer <algauer.henrique@gmail.com>
2023-05-17 16:23:31 -0300

    att readme

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -5,7 +5,7 @@ function Contact(){
             <div className=' p-16 flex w-6/12 text-zinc-100 bg-sky-300 flex-col items-center gap-8 '>
               <h2 className='text-5xl font-bold '>Vamos conversar</h2>
               <p className='text-xl text-center'>Faça alguma pergunta ou só deixe <br /> uma mensagem pra dizer oi :)</p>
-              <button className='font-bold border p-4'><a href={urlZap} target="_blank">WhatsApp</a></button>
+              <button className='font-bold border p-4 hover:bg-zinc-700 transition'><a href={urlZap} target="_blank">WhatsApp</a></button>
             </div>
           </div>
     )
